e578ddf990c89f766edcec8324d66ca825422630
Benny214 <benny.moralez@gmail.com>
2015-07-03 19:54:23 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,15 @@
+// $(document).ready(function() {
+// 	// position=0;
+// 	// var interval = setInterval(function () {
+// 	// position -= 1;
+// 	// $(".wrap").css({ "background-position":+ position +"px 0px" }) },1 );
+// });
+
+
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut(40000);
+	$(".loader").delay(400).fadeOut("slow");
+
+});
+
