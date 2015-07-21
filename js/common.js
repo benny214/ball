940f27c83860ce2c534ffcbbaf06fc97b574b6e9
Benny214 <benny.moralez@gmail.com>
2015-07-22 01:56:57 +0300

    stage2010

--- js/common.js
+++ js/common.js
@@ -1,31 +1,31 @@
- // $(document).ready(function() {
+ $(document).ready(function() {
 
- // 	$('#c_stage-wrap2').click(function(){
- // 		$('.c_stage-wrap2').addClass('active');
- // 		$('.c_stage-wrap1').removeClass('active');
- // 	});
- // 	$('#c_stage-wrap3').click(function(){
- // 		$('.c_stage-wrap3').addClass('active');
- // 		$('.c_stage-wrap2').removeClass('active');
- // 	});
- // 	$('#c_stage-wrap4').click(function(){
- // 		$('.c_stage-wrap4').addClass('active');
- // 		$('.c_stage-wrap3').removeClass('active');
- // 	});
- // 	$('#c_stage-wrap5').click(function(){
- // 		$('.c_stage-wrap5').addClass('active');
- // 		$('.c_stage-wrap4').removeClass('active');
- // 	});
- // 	$('#c_stage-wrap6').click(function(){
- // 		$('.c_stage-wrap6').addClass('active');
- // 		$('.c_stage-wrap5').removeClass('active');
- // 	});
- // 	$('#c_stage-wrap7').click(function(){
- // 		$('.c_stage-wrap7').addClass('active');
- // 		$('.c_stage-wrap6').removeClass('active');
- // 	});
+ 	$('#c_stage-wrap2').click(function(){
+ 		$('.c_stage-wrap2').addClass('active');
+ 		$('.c_stage-wrap1').removeClass('active');
+ 	});
+ 	$('#c_stage-wrap3').click(function(){
+ 		$('.c_stage-wrap3').addClass('active');
+ 		$('.c_stage-wrap2').removeClass('active');
+ 	});
+ 	$('#c_stage-wrap4').click(function(){
+ 		$('.c_stage-wrap4').addClass('active');
+ 		$('.c_stage-wrap3').removeClass('active');
+ 	});
+ 	$('#c_stage-wrap5').click(function(){
+ 		$('.c_stage-wrap5').addClass('active');
+ 		$('.c_stage-wrap4').removeClass('active');
+ 	});
+ 	$('#c_stage-wrap6').click(function(){
+ 		$('.c_stage-wrap6').addClass('active');
+ 		$('.c_stage-wrap5').removeClass('active');
+ 	});
+ 	$('#c_stage-wrap7').click(function(){
+ 		$('.c_stage-wrap7').addClass('active');
+ 		$('.c_stage-wrap6').removeClass('active');
+ 	});
  	
- // });
+ });
 
 
 $(window).load(function() {
